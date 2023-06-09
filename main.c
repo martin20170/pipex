c95b4054a39de18f86905c4d02a17ca480bc3889
Martin <martin_philip@hotmail.com>
2023-06-09 19:07:49 +0200

    working version?????

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mphilip < mphilip@student.42lyon.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 22:59:46 by mphilip           #+#    #+#             */
-/*   Updated: 2023/06/08 20:50:08 by mphilip          ###   ########.fr       */
+/*   Updated: 2023/06/09 17:59:28 by mphilip          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -119,75 +119,102 @@ int	main(int argc, char **argv)
 	char	**in_n_out = get_in_and_out(argv);
 	char	***cmds = get_cmds(argv);
 
-	pid_t	pid1;
-	pid_t	pid2;
+	pid_t	main_p;
+	pid_t	cmds_p;
+	pid_t	step1_p;
+	pid_t	step2_p;
 
-	pid1 = fork();
-	if (pid1 == 0)
+	main_p = fork();
+	if (main_p == 0)
 	{
 		int	fd[]={3, 4};
 		pipe(fd);
-		pid2 = fork();
-		if (pid2 == 0)
+		cmds_p = fork();
+		if (cmds_p == 0)
 		{
 			close(fd[0]);
-			int	infile = open(in_n_out[0], O_RDONLY);
-			if (infile == -1)
-				exit(0);
-			if (dup2(infile, STDIN_FILENO) == -1)
-				exit(0);
-			close(infile);
-			if (dup2(fd[1], STDOUT_FILENO) == -1)
-				exit(0);
 
 			char	*cmd1_name = cmds[0][0];
-			// if (execve(ft_strjoin("/bin/", cmd1_name), cmds[0], NULL) == -1)
-			// 	perror("\n\nPAS CE PATH LA\n\n");
-			if (execve(ft_strjoin("/bin/", cmd1_name), cmds[0], NULL) == -1)
-				perror("\n\ncommand not exist :)\n\n");
-			close(fd[1]);
+			char	*path1 = ft_strjoin("/usr/bin/", cmd1_name);
+			char	*path2 = ft_strjoin("/bin/", cmd1_name);
+			int		step_1_status = 0;
+
+			step1_p = fork();
+
+			if (step1_p == 0)
+			{
+				int	infile = open(in_n_out[0], O_RDONLY);
+				if (infile == -1)
+					exit(0);
+				if (dup2(infile, STDIN_FILENO) == -1)
+					exit(0);
+				close(infile);
+				if (dup2(fd[1], STDOUT_FILENO) == -1)
+					exit(0);
+				if (execve(path1, cmds[0], NULL) == -1 && execve(path2, cmds[0], NULL) == -1)
+					exit(EXIT_FAILURE);
+			}
+			else
+			{
+				if (waitpid(step1_p, &step_1_status, 0) == -1)
+					exit(EXIT_FAILURE);
+				if (WEXITSTATUS(step_1_status) == EXIT_FAILURE)
+					ft_printf("command not found : %s\n", cmd1_name);
+
+				free(path1);
+				free(path2);
+				close(fd[1]);
+				while (wait(NULL) != -1 || errno != ECHILD);
+			}
 		}
 		else
 		{
-			wait(NULL);
+			// if (waitpid(cmds_p, &cmds_status, 0) == -1)
+			// 	exit(EXIT_FAILURE);
+			// if (!WIFEXITED(cmds_status))
+			// 	exit(EXIT_FAILURE);
+
 			close(fd[1]);
-			int	outfile = open(in_n_out[1], O_WRONLY | O_CREAT, 0777);
-			if (outfile == -1)
-				exit(0);
-			if (dup2(outfile, STDOUT_FILENO) == -1)
-				exit(0);
-			close(outfile);
-			if (dup2(fd[0], STDIN_FILENO) == -1)
-				exit(0);
 
 			char	*cmd2_name = cmds[1][0];
-			// if (execve(ft_strjoin("/bin/", cmd2_name), cmds[1], NULL) == -1)
-			// 	perror("\n\nPAS CE PATH LA\n\n");
-			if (execve(ft_strjoin("/usr/bin/", cmd2_name), cmds[1], NULL) == -1)
-				perror("\n\ncommand not exist :)\n\n");
-			close(fd[0]);
+			char	*path1 = ft_strjoin("/usr/bin/", cmd2_name);
+			char	*path2 = ft_strjoin("/bin/", cmd2_name);
+			int		step_2_status = 0;
+
+			step2_p = fork();
+
+			if (step2_p == 0)
+			{
+				int	outfile = open(in_n_out[1], O_WRONLY | O_CREAT, 0777);
+				if (outfile == -1)
+					exit(0);
+				if (dup2(outfile, STDOUT_FILENO) == -1)
+					exit(0);
+				close(outfile);
+				if (dup2(fd[0], STDIN_FILENO) == -1)
+					exit(0);
+				if (execve(path1, cmds[1], NULL) == -1 && execve(path2, cmds[1], NULL) == -1)
+					exit(EXIT_FAILURE);
+			}
+			else
+			{
+				if (waitpid(step2_p, &step_2_status, 0) == -1)
+					exit(EXIT_FAILURE);
+				if (WEXITSTATUS(step_2_status) == EXIT_FAILURE)
+					ft_printf("command not found : %s\n", cmd2_name);
+
+				free(path1);
+				free(path2);
+				close(fd[1]);
+				while (wait(NULL) != -1 || errno != ECHILD);
+			}
 		}
 	}
 	else
 	{
-		waitpid(-1, NULL, 0);
+		while (wait(NULL) != -1 || errno != ECHILD);
 		free(in_n_out);
 		free_cmds(cmds);
-		ft_printf("\n\nFINISH\n\n");
 	}
 	return (0);
 }
-
-
-
-// ./pipex infile "grep -o '[[:alpha:]]'" "wc -l" outfile
-
-
-// char	*arg[4];
-// 	arg[0] = "a.out";
-// 	arg[1] = "-n";
-// 	arg[2] = "infie";
-// 	arg[3] = NULL;
-// 	if (execve("/bin/cat", arg, NULL) == -1)
-// 		perror("Erreur lors de l'ouverture du fichier");
-// 	ft_printf("ligne jamais atteinte\n");
--- test.c
+++
@@ -1,8 +0,0 @@
-#include "libft/libft.h"
-
-
-int	main(void)
-{
-	ft_printf("ca a marche!\n");
-	return (0);
-}
