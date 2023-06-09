ed4bffa099f06bf459e837e645db528a3901bb4a
Martin <martin_philip@hotmail.com>
2023-06-10 01:13:20 +0200

    exec loop and all path program working

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mphilip < mphilip@student.42lyon.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 22:59:46 by mphilip           #+#    #+#             */
-/*   Updated: 2023/06/09 17:59:28 by mphilip          ###   ########.fr       */
+/*   Updated: 2023/06/10 01:06:53 by mphilip          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,10 +49,6 @@ char	***get_cmds(char **input)
 	return (result);
 }
 
-// [[cat, -n], [wc, -l]]
-// tab[0] = [cat, -n]
-// tab[0][0] = cat
-// tab[0][0][0] = c
 
 
 void	free_cmds(char ***cmds)
@@ -75,49 +71,117 @@ void	free_cmds(char ***cmds)
 	free(cmds);
 }
 
-char	*complete_output(char *incomplete, char *to_add)
+void	free_strlst(char **var)
+{
+	int	i;
+
+	i = 0;
+	while (var[i])
+	{
+		free(var[i]);
+		i++;
+	}
+	free(var);
+}
+
+char	**get_path(char **envp)
+{
+	int		i;
+	char	**var;
+	char	**result;
+
+	i = 0;
+	while (envp[i])
+	{
+		var = ft_split(envp[i], '=');
+		if (ft_memcmp(var[0], "PATH\0", 5) == 0)
+		{
+			result = ft_split(var[1], ':');
+			if (!result)
+				return (free_strlst(var), NULL);
+
+			return (free_strlst(var), result);
+		}
+		free_strlst(var);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*complete_border(char *str)
 {
 	char	*result;
 
-	result = ft_strjoin(incomplete, to_add);
+	result = ft_strjoin("/", str);
 	if (!result)
 		return (NULL);
-	free(incomplete);
-	free(to_add);
 	return (result);
 }
 
-char	*get_cmd_result(int fd)
+char	**get_cmd_path(char **path, char *cmd_name)
 {
+	char	**result;
 	char	*temp;
-	char	*temp_result;
+	int		len;
+	int		i;
 
-	temp_result = malloc(1);
-	if (!temp_result)
+	i = 0;
+	len = 0;
+	while (path[len])
+		len++;
+	result = malloc(sizeof(char *) * (len + 1));
+	if (!result)
 		return (NULL);
-	temp_result[0] = 0;
-	temp = get_next_line(fd);
-	if (!temp)
-		return (free(temp_result), NULL);
-	while (temp[0])
+	while (i < len)
 	{
-		temp_result = complete_output(temp_result, temp);
-		if (!temp_result)
-			return (free(temp), free(temp_result), NULL);
-		temp = get_next_line(fd);
+		temp = complete_border(cmd_name);
 		if (!temp)
-			return (free(temp), free(temp_result), NULL);
+			return (free_strlst(result), NULL);
+		result[i] = ft_strjoin(path[i], temp);
+		if (!result)
+			return (free(temp), free_strlst(result), NULL);
+		i++;
 	}
-	return (free(temp), temp_result);
+	result[i] = NULL;
+	return (result);
 }
 
-int	main(int argc, char **argv)
+
+int	exec_loop(char **cmd_path, char **cmd)
+{
+	int		i;
+
+	i = 0;
+	while (cmd_path[i])
+	{
+		execve(cmd_path[i], cmd, NULL);
+		i++;
+	}
+	return (-1);
+}
+
+void	print_strlst(char **lst)
+{
+	int	i;
+
+	i = 0;
+	while (lst[i])
+	{
+		ft_printf("%s\n", lst[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv, char **envp)
 {
 	if (argc != 5)
 		error_state();
 
 	char	**in_n_out = get_in_and_out(argv);
 	char	***cmds = get_cmds(argv);
+	char	**path = get_path(envp);
+	if (!path)
+		exit(EXIT_FAILURE);
 
 	pid_t	main_p;
 	pid_t	cmds_p;
@@ -134,9 +198,7 @@ int	main(int argc, char **argv)
 		{
 			close(fd[0]);
 
-			char	*cmd1_name = cmds[0][0];
-			char	*path1 = ft_strjoin("/usr/bin/", cmd1_name);
-			char	*path2 = ft_strjoin("/bin/", cmd1_name);
+			char	**cmd1_path = get_cmd_path(path, cmds[0][0]);
 			int		step_1_status = 0;
 
 			step1_p = fork();
@@ -151,7 +213,7 @@ int	main(int argc, char **argv)
 				close(infile);
 				if (dup2(fd[1], STDOUT_FILENO) == -1)
 					exit(0);
-				if (execve(path1, cmds[0], NULL) == -1 && execve(path2, cmds[0], NULL) == -1)
+				if (exec_loop(cmd1_path, cmds[0]) == -1)
 					exit(EXIT_FAILURE);
 			}
 			else
@@ -159,26 +221,18 @@ int	main(int argc, char **argv)
 				if (waitpid(step1_p, &step_1_status, 0) == -1)
 					exit(EXIT_FAILURE);
 				if (WEXITSTATUS(step_1_status) == EXIT_FAILURE)
-					ft_printf("command not found : %s\n", cmd1_name);
+					ft_printf("command not found : %s\n", cmds[0][0]);
 
-				free(path1);
-				free(path2);
 				close(fd[1]);
+				free_strlst(cmd1_path);
 				while (wait(NULL) != -1 || errno != ECHILD);
 			}
 		}
 		else
 		{
-			// if (waitpid(cmds_p, &cmds_status, 0) == -1)
-			// 	exit(EXIT_FAILURE);
-			// if (!WIFEXITED(cmds_status))
-			// 	exit(EXIT_FAILURE);
-
 			close(fd[1]);
 
-			char	*cmd2_name = cmds[1][0];
-			char	*path1 = ft_strjoin("/usr/bin/", cmd2_name);
-			char	*path2 = ft_strjoin("/bin/", cmd2_name);
+			char	**cmd2_path = get_cmd_path(path, cmds[1][0]);
 			int		step_2_status = 0;
 
 			step2_p = fork();
@@ -193,7 +247,7 @@ int	main(int argc, char **argv)
 				close(outfile);
 				if (dup2(fd[0], STDIN_FILENO) == -1)
 					exit(0);
-				if (execve(path1, cmds[1], NULL) == -1 && execve(path2, cmds[1], NULL) == -1)
+				if (exec_loop(cmd2_path, cmds[1]) == -1)
 					exit(EXIT_FAILURE);
 			}
 			else
@@ -201,11 +255,10 @@ int	main(int argc, char **argv)
 				if (waitpid(step2_p, &step_2_status, 0) == -1)
 					exit(EXIT_FAILURE);
 				if (WEXITSTATUS(step_2_status) == EXIT_FAILURE)
-					ft_printf("command not found : %s\n", cmd2_name);
+					ft_printf("command not found : %s\n", cmds[1][0]);
 
-				free(path1);
-				free(path2);
 				close(fd[1]);
+				free_strlst(cmd2_path);
 				while (wait(NULL) != -1 || errno != ECHILD);
 			}
 		}
@@ -215,6 +268,7 @@ int	main(int argc, char **argv)
 		while (wait(NULL) != -1 || errno != ECHILD);
 		free(in_n_out);
 		free_cmds(cmds);
+		free_strlst(path);
 	}
 	return (0);
 }
